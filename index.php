f0ada0effe7a8e63d59a6ce9c6108fec1038538e
denermodanez <93441776+denermodanez@users.noreply.github.com>
2026-05-12 18:44:25 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+    <meta name="author" content="Eder Modanez">
+    <meta name="description" content="Atelie Gigante Guerreiro">
+	<meta name="keywords" content="Atelie, Gigante, Guerreiro">
+    <meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Ateli&ecirc; Gigante Guerreiro</title>
+	<link rel="icon" type="image/jpg" href="imgs/favicon.png" />
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+	<link href="css/estilo.css" rel="stylesheet" type="text/css" />
+	<link rel="manifest" href="manifest.json">
+    <script src="js/jquery-3.6.0.min.js" type="text/javascript"></script>
+</head>
+<body>
+    Teste
+</body>
+</html>
